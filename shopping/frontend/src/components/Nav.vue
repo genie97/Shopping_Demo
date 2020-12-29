58fad9900d5dcd38c99161f1ec3f56c261b7d134
genie97 <hellogenie97@gmail.com>
2020-12-29 23:08:37 +0900

    feat/home: Update Home UI

---
+++ shopping/frontend/src/components/Nav.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <v-card color="#FFEAEA">
+      <v-card-title class="text-center justify-center py-6">
+          <h2 class="font-weight-bold">Shopping Mall Demo</h2>
+      </v-card-title>
+      <v-tabs right>
+        <v-tab>Sign In</v-tab>
+        <v-tab>Sign Up</v-tab>
+        <v-tab>Cart</v-tab>
+      </v-tabs>
+    </v-card>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Nav"
+}
+</script>
+
+<style scoped>
+
+</style>
--- shopping/frontend/src/views/Home.vue
+++ shopping/frontend/src/views/Home.vue
@@ -1,18 +1,55 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <v-container fluid>
+      <v-row dense>
+        <v-col
+          v-for="card in cards"
+          :key="card.title"
+          :cols="card.flex"
+        >
+          <v-card>
+            <v-img
+              :src="card.src"
+              class="white--text align-end"
+              gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
+              height="200px"
+            >
+              <v-card-title v-text="card.title"></v-card-title>
+            </v-img>
+
+            <v-card-actions>
+              <v-spacer></v-spacer>
+
+              <v-btn icon>
+                <v-icon>mdi-cart</v-icon>
+              </v-btn>
+
+              <v-btn icon>
+                <v-icon>mdi-credit-card-check-outline</v-icon>
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
   name: "Home",
   components: {
-    HelloWorld
-  }
+
+  },
+  data: () => ({
+    cards: [
+      { title: 'Pre-fab homes', src: 'https://cdn.vuetifyjs.com/images/cards/house.jpg', flex: 6 },
+      { title: 'Favorite road trips', src: 'https://cdn.vuetifyjs.com/images/cards/road.jpg', flex: 6 },
+      { title: 'Best airlines', src: 'https://cdn.vuetifyjs.com/images/cards/plane.jpg', flex: 6 },
+      { title: 'Favorite road trips', src: 'https://cdn.vuetifyjs.com/images/cards/road.jpg', flex: 6 },
+      { title: 'Best airlines', src: 'https://cdn.vuetifyjs.com/images/cards/plane.jpg', flex: 6 },
+    ],
+  }),
 };
 </script>
